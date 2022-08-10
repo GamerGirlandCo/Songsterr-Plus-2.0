469a3b0ce86a53ecd0a19fdd7e90bc9a5c13cd25
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2022-08-10 14:54:08 -0400

    fix: rebase onto latest. fixes #7

--- src/inject.js
+++ src/inject.js
@@ -3,7 +3,7 @@ function bab() {
 	let s = document.createElement("script");
 	s.setAttribute("async", "");
 	s.setAttribute("type", "module");
-	s.setAttribute("src", browser.extension.getURL("latest/index.3637aaf7.js"));
+	s.setAttribute("src", browser.extension.getURL("latest/index.26929406.js"));
 	s.setAttribute("crossorigin", "anonymous")
 	document.body.appendChild(s)
 	let app = document.getElementById("app");
