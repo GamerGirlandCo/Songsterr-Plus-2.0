599babfa8713b0430b91c30b9088c3b22a13e1f2
Andreas Kurka <andreas.kurka@gmail.com>
2022-08-29 20:01:06 +0200

    Updated to use the correct file version

--- src/inject.js
+++ src/inject.js
@@ -3,7 +3,7 @@ function bab() {
 	let s = document.createElement("script");
 	s.setAttribute("async", "");
 	s.setAttribute("type", "module");
-	s.setAttribute("src", browser.extension.getURL("latest/index.26929406.js"));
+	s.setAttribute("src", browser.extension.getURL("latest/index.b0563877.js"));
 	s.setAttribute("crossorigin", "anonymous")
 	document.body.appendChild(s)
 	let app = document.getElementById("app");
