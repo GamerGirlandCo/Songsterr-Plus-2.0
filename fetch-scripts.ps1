b7d963d42e7c8870a4079b0e845888b178d0d2f1
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2022-12-30 19:40:45 -0500

    update fetcher

--- fetch-scripts.ps1
+++ fetch-scripts.ps1
@@ -1,5 +1,12 @@
-foreach($b in $(Get-ChildItem -Path src/latest *Panel*)) {
-    Write-Output $b;
-    $f=$b.Name
-    curl -L "https://www.songsterr.com/static/latest/$f" > src/latest/$f
+$importReg = "(await )?import\(['`"]\./(?<file>.*)['`"]\)"
+Get-Content ./src/latest/index.*.js | ForEach-Object {
+	if($_ -match $importReg) {
+		Write-Output $_
+		$blip = Select-String $importReg -InputObject $_
+		Write-Output -InputObject $blip
+		$fn = $blip.Matches[0].Groups["file"]
+		$f = $fn.Value
+		Write-Output $fn
+		curl -L "https://www.songsterr.com/static/latest/$f" > src/latest/$f
+	}
 }
