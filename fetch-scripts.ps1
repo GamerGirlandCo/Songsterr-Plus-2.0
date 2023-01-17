1bfac443b2e2bad09fe2e092ac6a8abf722cd201
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2023-01-17 17:28:44 -0500

    improve fetch script

--- fetch-scripts.ps1
+++ fetch-scripts.ps1
@@ -1,5 +1,16 @@
-$importReg = "(await )?import\(['`"]\./(?<file>.*)['`"]\)"
+$importReg = "(await )?import\(['`"]\./(?<file>.*)['`"]\)|import(?: )?['`"]\./(?<file>.*?)['`"];|import(?: )?\{.*\}(?: )?from(?: )?['`"]\./(?<file>.*)['`"]"
 Get-Content ./src/latest/index.*.js | ForEach-Object {
+	if($_ -match $importReg) {
+		# Write-Output $_
+		$blip = Select-String $importReg -InputObject $_
+		# Write-Output -InputObject $blip
+		$fn = $blip.Matches[0].Groups["file"]
+		$f = $fn.Value
+		# Write-Output $fn
+		curl -L "https://www.songsterr.com/static/latest/$f" > src/latest/$f
+	}
+}
+Get-Content ./src/latest/*.js | ForEach-Object {
 	if($_ -match $importReg) {
 		# Write-Output $_
 		$blip = Select-String $importReg -InputObject $_
