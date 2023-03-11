7b1b99e5aeb30beb70e4d5ab5ff3b61774aeb39a
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2023-03-11 16:46:03 -0500

    :)

--- src/blocknreplace.js
+++ src/blocknreplace.js
@@ -1,5 +1,7 @@
-const ast = require("abstract-syntax-tree/index.js")
+const {parse, generate, traverse, replace} = require("abstract-syntax-tree/index.js")
+const ast = require("abstract-syntax-tree")
 const browser = require("webextension-polyfill")
+const beautify = require('js-beautify').js
 
 browser.webRequest.onBeforeRequest.addListener(
 		function shruggie(details) {
@@ -13,8 +15,50 @@ browser.webRequest.onBeforeRequest.addListener(
 				source += decoder.decode(event.data, {stream: true})
 			}
 			filter.onstop = event => {
-				const tree = new ast(source, {webCompat: true})
+				source = beautify(source, {indent_size: 2, space_in_empty_paren: true})
+				let asArray = source.split("\n")
+				let joined = asArray.join("\n")
+				const tree = parse(joined, {webcompat: true, next: true, module: true})
 				console.log(tree)
+				let identifier;
+				traverse(tree, {
+					enter(node) {
+						if(node.type == "FunctionDeclaration") {
+							let body0 = node.body.body[0]
+							if(body0) {
+								if(body0.type === "IfStatement") {
+									console.log("cnode", node)
+									console.log("b0", body0)
+										if(body0.test.callee) {
+											identifier = body0.test.callee.name
+										}
+									}
+								}
+							}
+					}
+				})
+				console.log("iden", identifier)
+				replace(tree, node => {
+					let finder = a => a.id.name === identifier
+					if(node.type === "VariableDeclaration" && node.kind == "var") {
+						const blip = node.declarations.find(finder)
+						if(!!blip) {
+							console.log("refereeeee", node)
+							node.declarations[node.declarations.findIndex(finder)].init.body = {
+								type: "Literal",
+								value: true
+							}
+						}
+					}
+					return node
+				})
+				filter.write(encoder.encode(generate(tree)))
+				filter.close()
 			}
-		}
+			return {}
+		},
+		{
+			urls: ["https://www.songsterr.com/static/latest/index.*.js"]
+		},
+		['blocking']
 	)
