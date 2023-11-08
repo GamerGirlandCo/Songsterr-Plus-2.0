d625eeb9f5f0f057eb373d8369d1dffd5e192ddf
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2023-11-08 17:43:36 -0500

    build script buffs

--- build.ps1
+++ build.ps1
@@ -3,4 +3,6 @@ Remove-Item ../web-ext-artifacts/ext.xpi
 node ../esbuild.mjs
 Copy-Item blocknreplace.js chrome/
 zip -r ..\web-ext-artifacts\ext.xpi . -x ./chrome/* -x *.pem -x *.crx
+zip -r ..\web-ext-artifacts\chrome-unpacked.zip ./chrome -x *.zip
+crx3 -p chrome.pem --crxURL "https://github.com/GamerGirlandCo/Songsterr-Plus-2.0/releases/download/latest/chrome.crx" -o ../web-ext-artifacts/chrome.crx chrome/
 Set-Location ..
