33c18feec7945d39868973bffccab312266e88c7
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2023-11-08 16:59:47 -0500

    update build script

--- build.ps1
+++ build.ps1
@@ -2,5 +2,5 @@ Set-Location build;
 Remove-Item ../web-ext-artifacts/ext.xpi
 node ../esbuild.mjs
 Copy-Item blocknreplace.js chrome/
-zip -r ..\web-ext-artifacts\ext.xpi . -x ./chrome/*
+zip -r ..\web-ext-artifacts\ext.xpi . -x ./chrome/* -x *.pem -x *.crx
 Set-Location ..
